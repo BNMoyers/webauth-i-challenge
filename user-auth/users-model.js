d1f7b61ca257bdef6cd0f4b707c7007e4d4bdfb1
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-11-11 14:49:51 -0600

    created helper methods

--- user-auth/users-model.js
+++ user-auth/users-model.js
@@ -2,8 +2,36 @@
 const db = require('../data/dbConfig')
 
 //exports
+module.exports = {
+    find,
+    findBy,
+    findById,
+    add
+}
 
 //helper methods
 function find() {
-    return db('users').select('id', 'username');
+    return db('users')
+            .select('id', 'username');
+}
+
+function findBy(filter) {
+    return db('users')
+            .where(filter);
+}
+
+functin findById(Id) {
+    return db('users')
+            .select ('id', 'username')
+            .where({ id })
+            .first();
+}
+
+function add(user) {
+    return db('users')
+            .insert(user, 'id')
+            .then(ids => {
+                const [id] = ids;
+                return findById(id);
+            });
 }
