68ed9e3e1823f09dc4b6404c4902d08dde5fe587
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-11-11 14:02:26 -0600

    fleshed out index

---
+++ index.js
@@ -0,0 +1,6 @@
+//imports and dependencies
+const server = require('./api/server');
+
+//server & port
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, ()=> console.log(`***running on port ${PORT}***`));
