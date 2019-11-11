df72596844ed0bdea2223e6ba6d62b35bf672058
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-11-11 14:53:53 -0600

    created users-router

--- user-auth/users-router.js
+++ user-auth/users-router.js
@@ -2,4 +2,15 @@
 const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 const Users = require('./users-model');
+const requiresAuth = require('../user-auth/requires-auth-middleware');
+
+router.get('/users', requiresAuth, (req, res) =>{
+    Users.find()
+         .then(users => {
+             res.json(users);
+         }) 
+         .catch(err => res.send(err));
+})
+
+module.exports = router;
 
